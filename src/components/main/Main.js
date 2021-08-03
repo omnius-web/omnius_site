caf3d7cace8d3d1fca2e2d9c9ca2006d1cec802a
omnius <root@ip-172-31-4-208.ap-northeast-2.compute.internal>
2021-08-03 10:15:01 +0000

    server

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -7,7 +7,7 @@ function Main(){
                                 <div className="tit" data-aos="fade-down"  data-aos-duration="2000">
                                         제작부터 유지보수까지<br />
                                         같이 고민하고 책임지는<br />
-                                        파트너가 되겠습니다.ok
+                                        파트너가 되겠습니다.ok123
                                 </div>
                                 <div className="desc" data-aos="fade-up" data-aos-duration="2000">
                                         OMNIUS는 거품없는 가격으로<br />
