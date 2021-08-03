66794e4db7400666e8f705bf2958cd342df39ddb
omnius <gs5m3011@gmail.com>
2021-08-03 14:47:36 +0900

    210803-ie-Check

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,12 +8,24 @@ import OmSwitch from "./OmSwitch";
 import Footer from "./Footer";
 import OmBerGer from "./omberger/OmBerGer";
 
+function ieChk(){
+        var agent = navigator.userAgent.toLowerCase();
+        if ( (navigator.appName === 'Netscape' && agent.indexOf('trident') !== -1) || (agent.indexOf("msie") !== -1)) {
+                // ie일 경우
+                console.log('ie임');
+        }else{
+                // ie가 아닐 경우
+                console.log('ie아님');
+        }
+}
+
 function Header(){
         const [lopath, setlopath] = useState('/');
         const [hdName, setHdNAme] = useState('');
         const [footName, setFootNAme] = useState('');
         useEffect(()=>{
                 window.scrollTo(0, 0);
+                ieChk();
                 if(lopath === '/' || lopath.pathname === '/'){
                         setHdNAme('main_header');
                         setFootNAme('main_footer');
