4672cdfc07e8e7c4f4b02bbed674ea178304b647
omnius <gs5m3011@gmail.com>
2021-08-03 19:24:55 +0900

    last

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -7,7 +7,7 @@ function Main(){
                                 <div className="tit" data-aos="fade-down"  data-aos-duration="2000">
                                         제작부터 유지보수까지<br />
                                         같이 고민하고 책임지는<br />
-                                        파트너가 되겠습니다.ppp22
+                                        파트너가 되겠습니다.
                                 </div>
                                 <div className="desc" data-aos="fade-up" data-aos-duration="2000">
                                         OMNIUS는 거품없는 가격으로<br />
