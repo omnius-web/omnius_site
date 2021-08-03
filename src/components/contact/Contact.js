211e0ef82d50fd63b06e9820a89939a4184adfcf
omnius <gs5m3011@gmail.com>
2021-08-04 08:48:18 +0900

    210804-contact-aos-edit

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -5,18 +5,22 @@ function Contact(){
                 <div className="contactwrap alltop">
                         <div className="tit" style={{backgroundImage:"url(/main.jpg)"}}><span data-aos="fade-down" data-aos-duration="1000">Contact</span></div>
 
-                        <div className="con" data-aos="fade-right" data-aos-duration="1000">
-                                <li>원하시는 디자인, 그리고 상상하는 모든 기능이 제작가능합니다.</li>
-                                <li>거품없는 가격으로 일한 만큼의 비용으로 제작해드리겠습니다.</li>
-                                <li>궁금한점이 있으시다면 언제든지 연락주세요.</li>
-                                <li>친절하고 상세하게 상담해드리겠습니다.</li>
+                        <div data-aos="fade-up" data-aos-duration="1000">
+                                <div className="con">
+                                        <li>원하시는 디자인, 그리고 상상하는 모든 기능이 제작가능합니다.</li>
+                                        <li>거품없는 가격으로 일한 만큼의 비용으로 제작해드리겠습니다.</li>
+                                        <li>궁금한점이 있으시다면 언제든지 연락주세요.</li>
+                                        <li>친절하고 상세하게 상담해드리겠습니다.</li>
+                                </div>
+
+                                <div className="con2">
+                                        <li><a href="tel:1800-5756">대표번호 : 1800-5756</a></li>
+                                        <li>E-Mail : gs5m@naver.com</li>
+                                        <li>카카오톡 ID : pigweb</li>
+                                </div>
                         </div>
 
-                        <div className="con2" data-aos="fade-left" data-aos-duration="1000">
-                                <li><a href="tel:1800-5756">대표번호 : 1800-5756</a></li>
-                                <li>E-Mail : gs5m@naver.com</li>
-                                <li>카카오톡 ID : pigweb</li>
-                        </div>
+                        
                         
                 </div>
         )
